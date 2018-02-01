7c7911b76e29e6cabaaa1efe7359dfc391523166
jpoz330 <jpoz0321@gmail.com>
2018-02-01 17:47:42 -0600

    part 5

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-product-page',
   templateUrl: './product-page.component.html',
   styleUrls: ['./product-page.component.css']
 })
 export class ProductPageComponent implements OnInit {
+
 
   constructor() { }
 
